c634c2f8c667dced4785b94372ce299340ff6252
Jasur <shermatovjasur800@gmail.com>
2025-04-16 10:36:36 +0500

    Some changes

--- Selenium/cooke_selenium.py
+++ Selenium/cooke_selenium.py
@@ -1,6 +1,8 @@
+# bu yerda session id orqali instagramga kirsa boladi yaniy
+# application ichiga cookes ichiga session_id ni olish kerak
+import os
 import time
-
-from selenium.webdriver.chrome import webdriver
+from selenium import webdriver
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.chrome.service import Service
 from webdriver_manager.chrome import ChromeDriverManager
@@ -12,11 +14,14 @@ options.add_argument("--user-agent=Mozilla/5.0 (Windows NT 10.0; Win64; x64) App
 driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()), options=options)
 wait = WebDriverWait(driver, 30)
 
+instagram_session_id=os.getenv("INSTAGRAM_SESSION_ID")
+
 
 try:
-    driver.get("https://excapital.com")
-    print(driver.get_cookies())
-    time.sleep(40)
+    driver.get("https://www.instagram.com")
+    driver.add_cookie({"name": "sessionid", "value": "19545839310%3AQZ8a493YovI4td%3A15%3AAYfmHpFN7DlRsk5Xyqids2VNN9vHWCwOgkdukMoWBA"})
+    driver.refresh()
+    time.sleep(100)
 except Exception as e:
     driver.save_screenshot("screenshot.png")
     print(f"An error occurred: {e}")
